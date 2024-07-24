585549fa9e4d21608fc36bbca004d11a22f43556
Logan <lobsterlogan43@yahoo.com>
2024-07-23 22:36:52 -0500

    Icons

---
+++ lua/customroles/professional/shared.lua
@@ -0,0 +1,7 @@
+--[[
+    Traitor team
+    - Gets a bullet-type damage buff
+    - Shop gets body armor, radar
+        - Can also buy all ground-spawning weapons and ammo for them
+    - Starts with 3 credits
+]]
